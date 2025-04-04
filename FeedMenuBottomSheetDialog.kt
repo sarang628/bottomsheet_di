46c51cc84ac391b456a7fac773b57c371f9e1244
sryang <sarang628@gmail.com>
2025-04-04 15:42:44 +0900

    compose function reorganize
    - rename function and file names to suit the function

--- FeedMenuBottomSheetDialog.kt
+++ FeedMenuBottomSheetDialog.kt
@@ -1,11 +1,11 @@
 package com.sarang.torang.di.bottomsheet_di
 
 import androidx.compose.runtime.Composable
-import com.sarang.torang.compose.bottomsheet.feed.FeedMenuBottomSheetDialog
+import com.sarang.torang.compose.bottomsheet.feed.FeedMenuModalBottomSheet
 
 fun provideFeedMenuBottomSheetDialog(): @Composable (reviewId: Int, onClose: () -> Unit, onReport: (Int) -> Unit, onDelete: (Int) -> Unit, onEdit: (Int) -> Unit) -> Unit =
     { reviewId, onClose, onReport, onDelete, onEdit ->
-        FeedMenuBottomSheetDialog(
+        FeedMenuModalBottomSheet(
             isExpand = true,
             reviewId = reviewId,
             onReport = { onReport.invoke(reviewId) },
