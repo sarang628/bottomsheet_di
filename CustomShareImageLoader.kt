95c8aa0bb6cfbbb4dc9dbd9b72ce3e1fb3ed26ac
sryang <sarang628@gmail.com>
2026-02-09 23:36:12 +0900

    add video review

--- CustomShareImageLoader.kt
+++ CustomShareImageLoader.kt
@@ -1,14 +1,18 @@
 package com.sarang.torang.di.bottomsheet_di
 
+import androidx.compose.ui.unit.dp
 import com.sarang.torang.compose.bottomsheet.share.components.ShareImageLoadType
+import com.sarang.torang.di.image.TorangAsyncImageData
 import com.sarang.torang.di.image.provideTorangAsyncImage
 
 val CustomShareImageLoader : ShareImageLoadType = {
     provideTorangAsyncImage().invoke(
-        it.modifier,
-        it.model.toString(),
-        it.width,
-        it.height,
-        it.contentScale
+        TorangAsyncImageData(
+            it.modifier,
+            it.model.toString(),
+            it.width ?: 30.dp,
+            it.height ?: 30.dp,
+            it.contentScale
+        )
     )
 }
